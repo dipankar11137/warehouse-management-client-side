6b42ceb099bc7420355c00938df4b47c399e84ec
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-04 01:44:17 +0600

    add item

--- src/Components/Pages/Home/AddItem/AddItem.js
+++ src/Components/Pages/Home/AddItem/AddItem.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddItem = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'http://localhost:5000/products'
+        console.log(url)
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
     return (
-        <div className='mt-5 pt-5'>
-            <h1>Add item</h1>
+
+        <div className='mt-5 pt-5 w-50 mx-auto'>
+            <h1 className='text-white'>Add item</h1>
+            <div >
+                <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='mb-2 p-2' placeholder='Name' {...register("name", { required: true, maxLength: 30 })} />
+                    <input className='mb-2 p-2' placeholder='Price' type="number" {...register("price")} required />
+                    <input className='mb-2 p-2' placeholder='Quantity' type="number" {...register("quantity")} required />
+                    <input className='mb-2 p-2' placeholder='Supplier Name' type="text" {...register("supplierName")} required />
+                    <textarea className='mb-2 p-2' placeholder='Description' {...register("description")} required />
+                    <input className='mb-2 p-2' placeholder='Img' type="text" {...register("img")} required />
+                    <input className='mb-2 p-2' placeholder='Date' type="text" {...register("date")} required />
+                    <input className=' mb-5 btn btn-primary' type="submit" value="Add new Item" />
+                </form>
+            </div>
         </div>
     );
 };
